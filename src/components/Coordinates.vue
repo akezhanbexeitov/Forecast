968c157f76767d60f98b273ee4dd160684350d24
Akezhan <akidulatini@gmail.com>
2023-05-03 22:14:14 +0600

    Rewrite css for the Coordinates component

--- src/components/Coordinates.vue
+++ src/components/Coordinates.vue
@@ -5,10 +5,33 @@
 </script>
 
 <template>
-    <section class="section-bottom">
-        <div
-            class="block-bottom"
-        >
+    <section v-if="weatherInfo?.weather" class="coordinates">
+        <div class="compass">
+            <img class="compass-image" src="/src/assets/img/compass.png" alt="Compass">
+        </div>
+
+        <div class="text-box">
+            <div class="longtitude">
+                <p class="heading">
+                    Longitude: {{ weatherInfo?.weather?.coord?.lon }}
+                </p>
+                <p class="description">
+                    Longitude measures distance east or west of the prime meridian.
+                </p>
+            </div>
+
+            <div class="latitude">
+                <p class="heading">
+                    Latitude: {{ weatherInfo?.weather?.coord?.lat }}
+                </p>
+                <p class="description">
+                    Latitude lines start at the equator (0 degrees latitude) and run east and west, parallel to the equator.
+                </p>
+            </div>
+        </div>
+
+    </section>
+        <!-- <div>
             <div  v-if="weatherInfo?.weather" class="block-bottom-inner">
                 <div class="block-bottom-pic pic-coords"></div>
                 <div class="block-bottom-texts">
@@ -30,6 +53,46 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </div> -->
 </template>
+
+<style scoped>
+    .coordinates {
+        width: 600px;
+        height: 250px;
+        padding: 20px;
+        display: flex;
+        align-items: center;
+        gap: 20px;
+        background: url('/src/assets/img/gradient-1.jpg') no-repeat 0% 0%;
+        background-size: cover;
+        border-radius: 5%;
+    }
+    .compass {
+        width: 20%;
+    }
+
+    .compass-image {
+        width: 100%;
+    }
+
+    .text-box {
+        width: 80%;
+    }
+
+    .longtitude {
+        margin-bottom: 20px;
+    }
+
+    .heading {
+        font-size: 20px;
+        font-weight: 700;
+        margin-bottom: 10px;
+    }
+
+    .description {
+        color: lightgrey;
+        font-size: 14px;
+        line-height: 1.4;
+    }
+</style>
