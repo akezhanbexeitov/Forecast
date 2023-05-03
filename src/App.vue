f59e833d54fce43e5fcba8bd194ec9ef2436a524
Akezhan <akidulatini@gmail.com>
2023-05-03 23:04:27 +0600

    Change the position of input and add header, nav elements

--- src/App.vue
+++ src/App.vue
@@ -29,9 +29,22 @@
             <div class="search">
                 <input type="text" class="input">
             </div>
+            <nav class="navigation">
+                <ul class="nav-list">
+                    <li class="item">
+                        <a class="link" href="#">Highlights</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="#">Coordinates</a>
+                    </li>
+                    <li class="item">
+                        <a class="link" href="#">Humidity</a>
+                    </li>
+                </ul>
+            </nav>
         </header>
         <main class="main">
-        
+            <WeatherSummary />
         </main>
     </div>
 
@@ -41,13 +54,33 @@
     @import './assets/styles/main';
 
     .wrapper {
-        border: 1px solid black;
+        border: 1px solid #FFF;
         max-width: 970px;
         margin: 0 auto;
         height: 800px;
         padding: 20px 15px;
     }
 
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 25px;
+    }
+
+    .navigation {
+        width: 35%;
+    }
+
+    .nav-list {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .nav-list .item .link {
+        text-decoration: none;
+    }
+    
     .search {
         position: relative;
         display: inline-block;
@@ -72,7 +105,8 @@
         padding: 16px;
         font-family: 'Inter', Arial, sans-serif;
         color: $white;
-        background-color: rgba(0, 0, 0, 0.75);
+        background: url('/src/assets/img/gradient-1.jpg') no-repeat 0% 0%;
+        background-size: cover;
         border-radius: 16px;
         border: none;
         outline: none;
